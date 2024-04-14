b421e4b017176b3a2fdd188abff30063598ae957
mos3doun26 <mos3doun26@gmail.com>
2024-04-14 08:59:58 +0200

    un comment some code

--- set_data.py
+++ set_data.py
@@ -19,29 +19,21 @@ def to_csv_file(data_fram, csv_file_name):
 
 if __name__ == "__main__":
     # scrap machine learning search
-    # ml_search = f"https://wuzzuf.net/search/jobs/?a=hpb&q=machine%20learning&start=0"
-    # df_lst = scrap_all(ml_search, 13)
-    # to_csv_file(meger_dfs(df_lst), "Machine learning search.csv")
+    ml_search = f"https://wuzzuf.net/search/jobs/?a=hpb&q=machine%20learning&start=0"
+    df_lst = scrap_all(ml_search, 13)
+    to_csv_file(meger_dfs(df_lst), "Machine learning search.csv")
 
     # scrap data analysis search
-    # data_analysis_search = f"https://wuzzuf.net/search/jobs/?a=navbg&q=data%20analysis&start=0"
-    # df_lst = scrap_all(data_analysis_search, 134)
-    # to_csv_file(meger_dfs(df_lst), "Data Analysis search.csv")
+    data_analysis_search = f"https://wuzzuf.net/search/jobs/?a=navbg&q=data%20analysis&start=0"
+    df_lst = scrap_all(data_analysis_search, 134)
+    to_csv_file(meger_dfs(df_lst), "Data Analysis search.csv")
 
     # # scrap data science search
-    # data_science_search = f"https://wuzzuf.net/search/jobs/?a=navbg%7Cspbg&q=data%20science&start=0"
-    # df_lst = scrap_all(data_science_search, 123)
-    # to_csv_file(meger_dfs(df_lst), "Data Science search.csv")
+    data_science_search = f"https://wuzzuf.net/search/jobs/?a=navbg%7Cspbg&q=data%20science&start=0"
+    df_lst = scrap_all(data_science_search, 123)
+    to_csv_file(meger_dfs(df_lst), "Data Science search.csv")
 
     # # scrap business intelligence search
     business_intelligence_search = f"https://wuzzuf.net/search/jobs/?a=navbg&q=business%20intelligence&start=0"
     df_lst = scrap_all(business_intelligence_search, 35)
     to_csv_file(meger_dfs(df_lst), "Business Intelligence search.csv")
-
-
-
-
-# ml_search = f"https://wuzzuf.net/search/jobs/?a=hpb&q=machine%20learning&start=0"
-# df_lst = scrap_all(ml_search, 13)
-# # print(df_lst)
-# meger_dfs(df_lst).to_csv("Machine learning.csv")
